013660b40f6fa765f2dea4c207a37f2544073838
Stephen Fluin <stephen.fluin@gmail.com>
2020-06-11 12:40:13 -0700

    add firebase dependency to include in build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase-error-repro';
+  root;
+  constructor(db: AngularFireDatabase) {
+    this.root = db.object('/');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyBxDW1Vy5g8GXyt6iY0S7nw_tRnjOJRvEo',
+      authDomain: 'sunstar-views.firebaseapp.com',
+      databaseURL: 'https://sunstar-views.firebaseio.com',
+      projectId: 'sunstar-views',
+      storageBucket: 'sunstar-views.appspot.com',
+      messagingSenderId: '213571294940',
+      appId: '1:213571294940:web:679930766d51f0f6930619',
+      measurementId: 'G-JSG6T6RZMH',
+    }),
+    AngularFireDatabaseModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
